00e163b1bfee22be475a57ced076f3b10cf5f8cb
aa <eee@gmail.com>
2018-10-29 08:46:07 -0200

    vaaai man klk

--- formulario.php
+++ formulario.php
@@ -4,17 +4,26 @@
   padding: 0;
   margin: 0;
 }
+#dadosEmpresa{
+
+}
+#textoCentral{
+  text-align: center;
+}
 </style>
 <body>
+
   <div class="container">
-    <form id="contact" action="" method="post">
+    <form id="cadastro" action="" method="post">
+<div id="textoCentral">
+
       <h3 style="">Cadastro</h3>
       <h4>Nos informe seus dados cadastrais</h4>
-      <div>
-          <h4>Ṕrimeiramente nos diga onde você mora:
-  </h4>
-          <p style="color: DAA520;"class="select">
-            <select class="budget" v-model="selection.member">
+
+</div>
+
+        <label for="">Cidade:</label><br>
+            <select class="cidade" v-model="selection.member">
               <option value="PVI">Paranavaí</option>
   						<option value="GRC">Guairaçá</option>
               <option value="MGA">Maringá</option>
@@ -23,27 +32,30 @@
   						<option value="CSC">Cascavel</option>
               <option value="CL">Campo Largo</option>
   						<option value="7">União da Vitória</option>
-              </div>
-      <fieldset>
-        <input placeholder="Email " type="text" tabindex="1" required autofocus>
-      </fieldset>
-      <fieldset>
-        <input type="email" tabindex="2" required>
-      </fieldset>
-        <fieldset>
-        <input  type="email" tabindex="2" required>
-      </fieldset>
-      <fieldset>
-        <input type="url" tabindex="4" required>
-      </fieldset>
-      <fieldset>
-        <textarea tabindex="5" required></textarea>
-      </fieldset>
-      <fieldset>
-        <button name="submit" type="submit" class="enviar" data-submit="...enviano...">Enviar</button>
-      </fieldset>
-    </form>
+            </select>
+<br>
+<div id="dadosEmpresa">
+        <label for=""></label><br>
+       <input class="w3-input w3-border" placeholder="Email" type="email" tabindex="1" required autofocus>
 
+      <label for=""></label><br>
+        <input class="w3-input w3-border" placeholder="Nome da empresa" type="text" tabindex="2" required>
 
-  </>
+    <label for=""></label><br>
+        <input  class="w3-input w3-border" placeholder="CNPJ" type="text" tabindex="3" required>
+
+      <label for=""></label><br>
+        <input class="w3-input w3-border" placeholder="Categoria" type="text" tabindex="4" required>
+
+      <label for=""></label><br>
+        <input class="w3-input w3-border" placeholder="URL da empresa" type="text" tabindex="5" required>
+
+      <label for=""></label><br>
+
+</form>
+
+<button name="submit" type="submit" class="enviar" data-submit="enviando">Enviar</button>
+
+</div>
+
 </body>
